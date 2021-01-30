81b44f22ca56559740383a9a7d2272e175268aac
Maria Eduarda Rodrigues Garcia <mariaedrgarcia@gmail.com>
2021-01-30 17:04:14 -0300

    teste heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('app-ejs', 'root', null, {
-    host: 'localhost',
+const DATABASE_URL = new Sequelize('de282ajrgsbrl1', 'iagurltrajxkrf', '4264cd01479ab5f5fd6bd1e9b6727fbcc3a003aab49be30bed47b04c56055544', {
+    host: 'ec2-52-21-247-176.compute-1.amazonaws.com',
     dialect: 'mysql'
 });
 
-module.exports = sequelize;
+module.exports = DATABASE_URL;
